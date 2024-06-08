3a4155e2f0d30e93de4d59d741092356d916e868
Federico Acosta <fede.acosta2396@gmail.com>
2024-06-08 19:51:13 -0300

    se cambio el nombre de las interfaces

--- obra_social_grupo6/src/main/java/quarkus/service/impl/AuthServiceImpl.java
+++ obra_social_grupo6/src/main/java/quarkus/service/impl/AuthServiceImpl.java
@@ -4,23 +4,22 @@ import io.quarkus.elytron.security.common.BcryptUtil;
 import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import quarkus.dto.LoginRequest;
 import quarkus.dto.RegisterRequest;
 import quarkus.entity.Usuario;
 import quarkus.exception.IncorrectUsernameOrPasswordException;
 import quarkus.exception.UserNotFoundException;
 import quarkus.exception.UsernameAlreadyExistsException;
-import quarkus.service.AuthService;
-import quarkus.service.UsuarioService;
+import quarkus.service.IAuthService;
+import quarkus.service.IUsuarioService;
 
 import java.time.Duration;
 
 @ApplicationScoped
-public class AuthServiceImpl implements AuthService {
+public class AuthServiceImpl implements IAuthService {
 
     @Inject
-    private UsuarioService usuarioService;
+    private IUsuarioService IUsuarioService;
 
     @Override
     public Usuario register(RegisterRequest RegisterRequest) {
@@ -36,13 +35,13 @@ public class AuthServiceImpl implements AuthService {
                 .direccion(RegisterRequest.direccion())
                 .rol("PACIENTE")
                 .build();
-        usuarioService.save(newUser);
+        IUsuarioService.save(newUser);
         return newUser;
     }
 
     @Override
     public String generateJwt(String username) {
-        var user = usuarioService.findByUsername(username).orElseThrow(() -> new UserNotFoundException("Usuario no encontrado"));
+        var user = IUsuarioService.findByUsername(username).orElseThrow(() -> new UserNotFoundException("Usuario no encontrado"));
         return Jwt.issuer("backend-obra-social-grupo-6")
                 .upn(user.getUsername())
                 .groups(user.getRol())
@@ -55,7 +54,7 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String authenticate(LoginRequest loginRequest) {
-        Usuario user = usuarioService.findByUsername(loginRequest.username()).orElseThrow(IncorrectUsernameOrPasswordException::new);
+        Usuario user = IUsuarioService.findByUsername(loginRequest.username()).orElseThrow(IncorrectUsernameOrPasswordException::new);
         if (!BcryptUtil.matches(loginRequest.password(), user.getPassword())) throw new IncorrectUsernameOrPasswordException();
 
         return generateJwt(loginRequest.username());
@@ -64,7 +63,7 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public boolean userExiste(String username) {
 
-        return usuarioService.findByUsername(username).isPresent();
+        return IUsuarioService.findByUsername(username).isPresent();
 
     }
 }
