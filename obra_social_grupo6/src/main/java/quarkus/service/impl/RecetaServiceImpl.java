20704c076e889f37ccb7263e46fc8269d690609a
NicoBrites <nicolas.britesv@gmail.com>
2024-06-09 16:48:33 -0300

    Agregado de validacion por fecha a Receta y Excepciones

--- obra_social_grupo6/src/main/java/quarkus/service/impl/RecetaServiceImpl.java
+++ obra_social_grupo6/src/main/java/quarkus/service/impl/RecetaServiceImpl.java
@@ -6,11 +6,13 @@ import quarkus.dto.RecetaDto;
 import quarkus.dto.mapper.RecetaMapper;
 import quarkus.entity.Turno;
 import quarkus.entity.Receta;
-import quarkus.exception.UserNotFoundException;
+import quarkus.exception.RecetaException;
+import quarkus.exception.TurnoException;
 import quarkus.repository.RecetaRepository;
 import quarkus.repository.TurnoRepository;
 import quarkus.service.IRecetaService;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
@@ -31,14 +33,20 @@ public class RecetaServiceImpl  implements IRecetaService{
 
         Optional<Turno> turnoOpcional = turnoRepository.findByIdOptional(idTurno);
         if (turnoOpcional.isEmpty()) {
-            throw new UserNotFoundException("Turno no encontrado");
+            throw new TurnoException("Turno no encontrado");
         }
         Turno entity = turnoOpcional.get(); 
 
         if (!entity.getPaciente().getUsername().toString().equals(jwt.getClaim("upn").toString()))  {
-            throw new UserNotFoundException("A este Paciente no le corresponde la receta de este Turno");
+            throw new RecetaException("A este Paciente no le corresponde la receta de este Turno");
         }
         Optional<Receta> recetaOpcion = recetaRepository.findByIdTurno(idTurno);
-        return recetaMapper.EntityToDto(recetaOpcion.get());
+
+        Receta receta =  recetaOpcion.get();
+        if (receta.getFechaValidez().isBefore(LocalDate.now())){
+            throw new RecetaException("Ya se vencio la fecha de validez de la Receta");
+        }
+
+        return recetaMapper.EntityToDto(receta);
     }
 }
