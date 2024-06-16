396369e354af1e7d8d6709c7700e8e809b859624
Federico Acosta <fede.acosta2396@gmail.com>
2024-06-16 19:05:32 -0300

    se agrego documentacion para swagger

--- obra_social_grupo6/src/main/java/quarkus/controller/AuthController.java
+++ obra_social_grupo6/src/main/java/quarkus/controller/AuthController.java
@@ -7,12 +7,26 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import quarkus.dto.LoginRequest;
 import quarkus.dto.RegisterRequest;
 import quarkus.service.IAuthService;
 
 @Path("/auth")
 @Produces(MediaType.TEXT_PLAIN)
+@APIResponses(
+        value = {
+                @APIResponse(
+                        responseCode = "500",
+                        description = "Error interno del servidor"),
+                @APIResponse(
+                        responseCode = "400",
+                        description = "Error en la solicitud verifique los datos enviados")
+
+        }
+)
+
 public class AuthController {
 
 
@@ -22,6 +36,16 @@ public class AuthController {
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     @PermitAll
+    @Path("register")
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "201",
+                            description = "Usuario Creado correctamente"),
+
+
+            }
+    )
     public Response register (RegisterRequest registerRequest){
             IAuthService.register(registerRequest);
             return Response.status(Response.Status.CREATED).entity("Usuario creado correctamente").build();
@@ -31,6 +55,13 @@ public class AuthController {
     @Path("login")
     @Produces(MediaType.TEXT_PLAIN)
     @PermitAll
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "200",
+                            description = "Logeado Correctamente")
+            }
+    )
     public Response login(LoginRequest loginRequest){
         var token = IAuthService.authenticate(loginRequest);
         return Response.ok(token).build();
